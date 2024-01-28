ee97a5204304934889298091b364e052108e3f93
Francisco Castillo <castillocerna@gmail.com>
2024-01-27 18:41:06 -0600

    Updated Shunting Yard Tests

--- tests/regex_tests/shunting_yard_tests.py
+++ tests/regex_tests/shunting_yard_tests.py
@@ -1,29 +1,29 @@
 import unittest
-from src.regex import shunting_yard as sy
-import src.regex.shunting_yard as sy_class
+from src.regex.shunting_yard import *;
+import src.regex.shunting_yard as sy
 
 
 class ShuntingYard(unittest.TestCase):
     def setUp(self):
-        self.sy = sy_class.ShuntingYard()
+        self.sy = sy.ShuntingYard()
 
     def test_get_alphabet(self):
         regex = 'a.b|c*.d'
         expected = {'a', 'b', 'c', 'd', 'ε'}
-        self.assertEqual(sy.get_alphabet(regex), expected)
+        self.assertEqual(get_alphabet(regex), expected)
 
         regex = 'a(a|b)*b'
         expected = {'a', 'b', 'ε'}
-        self.assertEqual(sy.get_alphabet(regex), expected)
+        self.assertEqual(get_alphabet(regex), expected)
 
     def test_insert_concat_operator(self):
         regex = 'a.b|c*.d'
         expected = 'a.b|c*.d'
-        self.assertEqual(sy.insert_concat_operator(regex), expected)
+        self.assertEqual(insert_concat_operator(regex), expected)
 
         regex = 'a(a|b)*b'
         expected = 'a.(a|b)*.b'
-        self.assertEqual(sy.insert_concat_operator(regex), expected)
+        self.assertEqual(insert_concat_operator(regex), expected)
 
     def test_get_postfix(self):
         regex = 'a(a|b)*b'
