967ea14ab779b975c47c8a2858d9cc99ab006d98
Francisco Castillo <castillocerna@gmail.com>
2024-04-25 19:58:01 -0600

    File Splitting

--- src/yapar/file_parser.py
+++ src/yapar/file_parser.py
@@ -1,32 +1,55 @@
-def read_file(file_path: str) -> str:
+def remove_comments(file_content: str) -> str:
     """
-    Read the content of a file and return it as a string (Except the comments)
+    Return the content of the file as a string (Except the comments)
     """
-    with open(file_path, "r") as file:
-        content = file.read()
-
-    # Remove comments
     result = ""
     in_comment = False
     i = 0
-    while i < len(content):
-        if content[i:i + 2] == '/*':
+    while i < len(file_content):
+        if file_content[i:i + 2] == '/*':
             in_comment = True
             i += 2
-        elif content[i:i + 2] == '*/':
+        elif file_content[i:i + 2] == '*/':
             if not in_comment:
-                result += content[i:i + 2]
+                result += file_content[i:i + 2]
             in_comment = False
             i += 2
         elif not in_comment:
-            result += content[i]
+            result += file_content[i]
             i += 1
         else:
             i += 1
     return result
 
 
+def split_file(file_content: str) -> tuple:
+    """
+    Split the content of a file into the tokens and productions sections
+    """
+    content = file_content
+    if not content:
+        raise Exception("Empty file")
+    content = remove_comments(content)
+    try:
+        tokens_content, productions_content = content.split("%%")
+    except ValueError:
+        raise Exception("Invalid file format. Missing %%")
+    return tokens_content.strip(), productions_content.strip()
+
+
 class FileParser:
     def __init__(self, file_path: str):
-        self.content = read_file(file_path)
-        print(self.content)
+        try:
+            # Read the file
+            with open(file_path, 'r') as file:
+                content = file.read()
+
+            # Split the file into tokens and productions
+            self.tokens_section, self.productions_section = split_file(content)
+            print(self.tokens_section)
+            print("================")
+            print(self.productions_section)
+
+
+        except Exception as e:
+            raise Exception(f"Error processing the YAPar file: {e}")
