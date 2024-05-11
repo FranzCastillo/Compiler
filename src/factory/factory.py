4019a5e1c8b8b322a351b8d4ca98f041ce03680f
Francisco Castillo <castillocerna@gmail.com>
2024-05-10 19:41:43 -0600

    Main Factory Implemented

--- src/factory/factory.py
+++ src/factory/factory.py
@@ -1,9 +1,11 @@
 from src.factory.yalex_factory import Factory as YalexFactory
 from src.factory.yapar_factory import Factory as YaparFactory
+from src.factory.main_factory import Factory as MainFactory
+
 
 def copy_token_file(output_path: str) -> None:
     with open("../structures/token.py", "r") as file:
-        with open(f"{output_path}/token.py", "w") as token_file:
+        with open(f"{output_path}/custom_token.py", "w") as token_file:
             token_file.write(file.read())
 
 
@@ -15,8 +17,16 @@ class Factory:
         self.yapar_path = yapar_path
         self.yap_factory = YaparFactory(yapar_path, output_path)
 
+        self.main_factory = MainFactory(output_path)
+
         self.output_path = output_path
 
     def create_analyzer(self):
+        self.main_factory.create_main()
+
+        # YALex Files
         copy_token_file(self.output_path)
         self.yal_factory.create_lex_analyzer()
+
+        # YAPar Files
+        self.yap_factory.create_parser()
